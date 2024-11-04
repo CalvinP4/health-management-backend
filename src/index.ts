396d8c9720c728721ff31eee48345c8a1d7ad129
CalvinP4 <calvincp28@gmail.com>
2024-11-04 11:39:09 -0500

    Create endpoint to add and delete slots

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import patientController from './entities/patient/controller/patientController';
 import doctorController from './entities/doctor/controller/doctorController';
 import hospitalController from './entities/hospital/controller/hospitalController';
@@ -13,8 +14,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors({ origin: 'http://localhost:3001' })); // Add this line to enable CORS
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/patient', patientController);
 app.use('/doctor', doctorController);
@@ -23,8 +25,7 @@ app.use('/appointment', appointmentController);
 app.use('/doctorDepartment', doctorDepartment);
 app.use('/department', departmentController);
 
-
-app.listen(port,async () => {
+app.listen(port, async () => {
     console.log(`Server is running on port ${port}`);
     await initDatabase();
 });
